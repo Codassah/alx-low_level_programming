1f81e90ebfde0574bc67380dcea84ac27820e6f5
codassah <boakyeboatengesther@gmail.com>
2022-09-11 16:43:31 -0700

    last two

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main: prints string in put function
+ * Description : code prints out and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * Return: 0
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
